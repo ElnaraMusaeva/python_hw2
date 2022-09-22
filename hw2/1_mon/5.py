54e3b6894e8f3428bdad2cd9d3755d52e51540fe
kolechko <98757986+ElnaraMusaeva@users.noreply.github.com>
2022-09-22 23:19:15 +0600

    Update 5.py

--- hw2/1_mon/5.py
+++ hw2/1_mon/5.py
@@ -1,12 +1,1 @@
-def func(a,b):
-    if a>2*b:
-        return 1
-    else:
-        f=a        
-    return f*func(a+1,b)
-n=int(input())
-sum=0
-for i in range(1,n+1):
-    b=i
-    sum+=func(i,b)
-print(sum)
+
